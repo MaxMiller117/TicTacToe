1219b7b9464560b4930aa581970686be829fe04a
MaxMiller117 <MaxMiller117@gmail.com>
2015-10-22 11:32:14 -0400

    init

---
+++ TicTacToeLogic.java
@@ -0,0 +1,26 @@
+
+public class TicTacToeLogic {
+	private int[][] board; //0 is blank, 1 is player 1's piece, 2 is player 2's piece
+	public int getCell(int x,int y){
+		return board[x][y];
+	}
+	public void setCell(int x,int y,int p){
+		board[x][y]=p;
+	}
+	public void setCell(int[] move){
+		setCell(move[0],move[1],move[2]);
+	}
+	public int isWin(){ //0 is no win, 1 is player 1 wins, 2 is player 2 wins
+		for(int i=0;i<3;i++){
+			if(board[i][0]==board[i][1] && board[i][0]==board[i][2]) //horizontal win condition
+				return board[i][0];
+			if(board[0][i]==board[1][i] && board[0][i]==board[2][i]) //vertical win condition
+				return board[0][i];
+		}
+		if(board[1][1]==board[0][0] && board[1][1]==board[2][2]) //downward diagonal win condition
+			return board[1][1];
+		if(board[1][1]==board[0][2] && board[1][1]==board[2][0]) //upward diagonal win condition
+			return board[1][1];
+		return 0;
+	}
+}
