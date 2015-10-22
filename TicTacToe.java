9187104831dbbfa18359db7733401c72203a2b80
MaxMiller117 <MaxMiller117@gmail.com>
2015-10-22 11:52:19 -0400

    Added interface and console

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,19 @@
 
 public class TicTacToe {
 	public static void main(String[] args){
-		TicTacToeGUI gui=new TicTacToeGUI();
+		TicTacToeInput gui=new TicTacToeGUI();
+		TicTacToeInput console=new TicTacToeCommandLineUI();
 		TicTacToeLogic logic=new TicTacToeLogic();
-		while(true){
-			int[] move=gui.getMove(logic);
+		int win=0;
+		while(win==0){
+			//int[] move=gui.getMove(logic);
+			int[] move=console.getMove(logic);
 			logic.setCell(move);
-			int win=logic.isWin();
+			win=logic.isWin();
 			if(win!=0){
 				System.out.println("Player "+win+" wins.");
 				//end program
+				//break
 			}
 		}
 	}
