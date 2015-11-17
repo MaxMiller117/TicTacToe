39eec0913e1d9f02a0ca00ef2a9568eb9d580352
MaxMiller117 <MaxMiller117@gmail.com>
2015-11-17 11:25:14 -0500

    Added win() to Input

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -1,7 +1,10 @@
 
-public class TicTacToeGUI {
+public class TicTacToeGUI implements TicTacToeInput {
 	public int[] getMove(TicTacToeLogic logic){
 		return new int[]{1,2,3};
 		//Will return move in form of {x,y,p}
+	}
+	public void win(int player){
+		
 	}
 }
