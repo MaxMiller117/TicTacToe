97f60262f2e9f99b5def642685363afdf5c2cf0a
MaxMiller117 <MaxMiller117@gmail.com>
2015-11-17 11:24:20 -0500

    Added win() to Input

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,14 +6,13 @@ public class TicTacToe {
 		TicTacToeLogic logic=new TicTacToeLogic();
 		int win=0;
 		while(win==0){
-			//int[] move=gui.getMove(logic);
-			int[] move=console.getMove(logic);
+			TicTacToeInput io=console;
+			int[] move=io.getMove(logic);
 			logic.setCell(move);
 			win=logic.isWin();
 			if(win!=0){
-				System.out.println("Player "+win+" wins.");
-				//end program
-				//break
+				io.win(win);
+				System.exit(0);
 			}
 		}
 	}
