22206d45aa6e1bbe55406d4b9fd120ddb14395ab
MaxMiller117 <MaxMiller117@gmail.com>
2015-11-17 11:36:16 -0500

    Win condition fix

--- TicTacToeLogic.java
+++ TicTacToeLogic.java
@@ -15,14 +15,14 @@ public class TicTacToeLogic {
 	}
 	public int isWin(){ //0 is no win, 1 is player 1 wins, 2 is player 2 wins
 		for(int i=0;i<3;i++){
-			if(board[i][0]==board[i][1] && board[i][0]==board[i][2]) //horizontal win condition
+			if(board[i][0]==board[i][1] && board[i][0]==board[i][2] && board[i][0]!=0) //horizontal win condition
 				return board[i][0];
-			if(board[0][i]==board[1][i] && board[0][i]==board[2][i]) //vertical win condition
+			if(board[0][i]==board[1][i] && board[0][i]==board[2][i] && board[i][0]!=0) //vertical win condition
 				return board[0][i];
 		}
-		if(board[1][1]==board[0][0] && board[1][1]==board[2][2]) //downward diagonal win condition
+		if(board[1][1]==board[0][0] && board[1][1]==board[2][2] && board[1][1]!=0) //downward diagonal win condition
 			return board[1][1];
-		if(board[1][1]==board[0][2] && board[1][1]==board[2][0]) //upward diagonal win condition
+		if(board[1][1]==board[0][2] && board[1][1]==board[2][0] && board[1][1]!=0) //upward diagonal win condition
 			return board[1][1];
 		return 0;
 	}
