ecebf63082772ba042e9d7d374aa9798e875d800
MaxMiller117 <MaxMiller117@gmail.com>
2015-11-24 10:58:20 -0500

    Adding AI support and package name

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -1,10 +1,11 @@
+package TicTacToe;
 
 public class TicTacToeGUI implements TicTacToeInput {
-	public int[] getMove(TicTacToeLogic logic){
-		return new int[]{1,2,3};
-		//Will return move in form of {x,y,p}
-	}
-	public void win(int player){
-		
-	}
+   public int[] getMove(TicTacToeLogic logic){
+      return new int[]{1,2,3};
+   	//Will return move in form of {x,y,p}
+   }
+   public void win(int player){
+   	
+   }
 }
